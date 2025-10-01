a7140f648b3a240f162a1b7ebf4ab1217ff56656
LeonardoBrusini <leonardo.brusini@mail.polimi.it>
2025-10-01 09:10:10 +0000

    optional text features on gather_features

--- src/open_clip/loss.py
+++ src/open_clip/loss.py
@@ -17,71 +17,59 @@ try:
 except ImportError:
     hvd = None
 
-
 def gather_features(
         image_features,
-        text_features,
+        text_features=None,
         local_loss=False,
         gather_with_grad=False,
         rank=0,
         world_size=1,
-        use_horovod=False
+        use_horovod=False,
 ):
     assert has_distributed, 'torch.distributed did not import correctly, please use a PyTorch version with support.'
     if use_horovod:
         assert hvd is not None, 'Please install horovod'
         if gather_with_grad:
             all_image_features = hvd.allgather(image_features)
-            all_text_features = hvd.allgather(text_features)
+            all_text_features = hvd.allgather(text_features) if text_features is not None else None
         else:
             with torch.no_grad():
                 all_image_features = hvd.allgather(image_features)
-                all_text_features = hvd.allgather(text_features)
+                all_text_features = hvd.allgather(text_features) if text_features is not None else None
             if not local_loss:
                 # ensure grads for local rank when all_* features don't have a gradient
                 gathered_image_features = list(all_image_features.chunk(world_size, dim=0))
-                gathered_text_features = list(all_text_features.chunk(world_size, dim=0))
                 gathered_image_features[rank] = image_features
-                gathered_text_features[rank] = text_features
                 all_image_features = torch.cat(gathered_image_features, dim=0)
-                all_text_features = torch.cat(gathered_text_features, dim=0)
+                if text_features is not None:
+                    gathered_text_features = list(all_text_features.chunk(world_size, dim=0))
+                    gathered_text_features[rank] = text_features
+                    all_text_features = torch.cat(gathered_text_features, dim=0)
     else:
         # We gather tensors from all gpus
         if gather_with_grad:
             all_image_features = torch.cat(torch.distributed.nn.all_gather(image_features), dim=0)
-            all_text_features = torch.cat(torch.distributed.nn.all_gather(text_features), dim=0)
+            if text_features is not None:
+                all_text_features = torch.cat(torch.distributed.nn.all_gather(text_features), dim=0)
+            else:
+                all_text_features = None
         else:
             gathered_image_features = [torch.zeros_like(image_features) for _ in range(world_size)]
-            gathered_text_features = [torch.zeros_like(text_features) for _ in range(world_size)]
             dist.all_gather(gathered_image_features, image_features)
-            dist.all_gather(gathered_text_features, text_features)
             if not local_loss:
                 # ensure grads for local rank when all_* features don't have a gradient
                 gathered_image_features[rank] = image_features
-                gathered_text_features[rank] = text_features
             all_image_features = torch.cat(gathered_image_features, dim=0)
-            all_text_features = torch.cat(gathered_text_features, dim=0)
+            if text_features is not None:
+                gathered_text_features = [torch.zeros_like(text_features) for _ in range(world_size)]
+                dist.all_gather(gathered_text_features, text_features)
+                if not local_loss:
+                    gathered_text_features[rank] = text_features
+                all_text_features = torch.cat(gathered_text_features, dim=0)
+            else:
+                all_text_features = None
 
     return all_image_features, all_text_features
-
-def gather_image_features(
-        image_features,
-        local_loss=False,
-        gather_with_grad=False,
-        rank=0,
-        world_size=1
-):
-    # We gather tensors from all gpus
-    if gather_with_grad:
-        all_image_features = torch.cat(torch.distributed.nn.all_gather(image_features), dim=0)
-    else:
-        gathered_image_features = [torch.zeros_like(image_features) for _ in range(world_size)]
-        dist.all_gather(gathered_image_features, image_features)
-        if not local_loss:
-            # ensure grads for local rank when all_* features don't have a gradient
-            gathered_image_features[rank] = image_features
-        all_image_features = torch.cat(gathered_image_features, dim=0)
-    return all_image_features
 
 def concat_all_gather(tensor: torch.Tensor) -> torch.Tensor:
     """
@@ -355,12 +343,13 @@ class StableRepPlusLoss(MultiCLIPLoss):
 
     def get_stablerep_logits(self, image_features, logit_scale=10, logit_bias=None):
         if self.world_size > 1:
-            all_image_features = gather_image_features(
+            all_image_features, _ = gather_features(
                 image_features,
                 local_loss=self.local_loss,
                 gather_with_grad=self.gather_with_grad,
                 rank=self.rank,
-                world_size=self.world_size
+                world_size=self.world_size,
+                use_horovod=self.use_horovod,
             )
 
             # StableRep originally uses a FIXED temperature of 0.1 and devides the logits, which is equivalent to multiplying by 10
